c973e90a64529e279f6fb41acc5d095111e494af
Christoph Urlacher <christoph.urlacher@protonmail.com>
2024-01-18 21:12:56 +0100

    Read file passed through argv[]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
+#include "lex.hpp"
+
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cout << "Usage: bfuck <IN-PATH>" << std::endl;
+    }
+    std::cout << "Running " << argv[1] << "...\n" << std::endl;
+
+    std::string tokens;
+    lex_brainfuck_file(argv[1], tokens);
+
+    return 0;
 }
